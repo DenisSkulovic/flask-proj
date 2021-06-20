9c9d4ccb0d6f70f4b471e7ba851977f0467721ed
DenisSkulovic <dskulovich@gmail.com>
2021-06-20 21:09:22 +0300

    stuff

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask import request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
-app.config
-
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = os.environ.get("SECRET_KEY")
 db = SQLAlchemy(app)
 
 
@@ -18,4 +20,6 @@ def page_not_found(e):
     ]), 404
 
 
-app.run()
+if __name__ == "__main__":
+    db.create_all()
+    app.run()
--- database/db.py
+++ database/db.py
@@ -2,6 +2,16 @@ from app import db
 from sqlalchemy.dialects.postgresql import JSON
 
 
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    pname = db.Column(db.String(80), unique=True, nullable=False)
+    color = db.Column(db.String(120), nullable=False)
+
+    def __init__(self, pname, color):
+        self.pname = pname
+        self.color = color
+
+
 class Result(db.Model):
     __tablename__ = 'results'
 
